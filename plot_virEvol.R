1bee65ee8ab53d9c2df8d271153371a7f26837c6
jsheen <jsheen@sas.upenn.edu>
2022-06-01 16:37:21 -0400

    draft plot of output showing synergistic effect of migration and vaccination

--- plot_virEvol.R
+++ plot_virEvol.R
@@ -4,9 +4,10 @@
 # ------------------------------------------------------------------------------
 library(ggplot2)
 library(reshape2)
+library(gridExtra)
 
-pdf('~/virEvol/output.pdf')
-par(mfrow = c(5, 5))
+l <- list()
+l_dex <- 1
 for (perc_sold_per_farm in seq(0, 0.8, by=0.2)) {
   for (perc_vax in seq(0, 0.8, by=0.2)) {
     pip <- read.csv(paste0('~/virEvol/res/', perc_sold_per_farm, '_', perc_vax, '.csv'))
@@ -14,17 +15,20 @@ for (perc_sold_per_farm in seq(0, 0.8, by=0.2)) {
     pip <- data.matrix(pip)
     pip <- ifelse((pip != 0 & pip != 1), NA, pip)
     colnames(pip) <- seq(0.01, 100, 5)
-    rownames(pip) <- seq(0.01, 100, 5)
+    rownames(pip) <- seq(95.01, 0.01, -5)
     melted <- melt(pip)
-    ggplot(melted, aes(x = Var2, y = Var1, fill = factor(value))) + geom_tile() +
+    colnames(melted) <- c('invader_virulence', 'resident_virulence', 'value')
+    temp_plot <- ggplot(melted, aes(y = invader_virulence, x = resident_virulence, fill = factor(value))) + geom_tile() +
       #scale_y_reverse() +
-      scale_fill_manual(values = c("black", "white"), na.value='grey')
+      scale_fill_manual(values = c("black", "white"), na.value='grey') + 
+      geom_abline(intercept = 0, slope = 1, col='red') +
+      theme(legend.position="none")
+    l[[l_dex]] <- temp_plot
+    l_dex <- l_dex + 1
   }
 }
-dev.off()
+ggsave(filename="~/virEvol/output.tiff", marrangeGrob(grobs = l, nrow=5, ncol=5, top='Percent sold per farm in 3 months -->',
+                                                     left='<-- Percent vaccinated in 3 months'),
+       width=8, height=8, units='in', dpi=600)
 
-pip <- matrix(finalMatrix, ncol=length(vir_steps), nrow=length(vir_steps), byrow=F)
-pip <- pracma::flipud(pip) #columns stay in place, but now from bottom to top is increasing virulence
-#pip <- ifelse((pip != 0 & pip != 1), NA, pip)
-#plot(pip)
-write.csv(pip, paste0('~/virEvol/res/', perc_sold_per_farm, '_', perc_vax, '.csv'))
+
